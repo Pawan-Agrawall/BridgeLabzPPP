fcfac0f2730f7aa4a45dc5ea5e3f497dcb92788b
Pawan Agrawal <pavanagrawalofficial1@gmail.com>
2025-08-20 10:49:10 +0530

    modify

--- CalculateSimpleInterest.java
+++
@@ -1,8 +0,0 @@
-public class CalculateSimpleInterest {
-    public static void main(String[] args) {
-        int principal = 1000;
-        int rate = 5; 
-        int time = 2; // time in years
-        System.out.println("Simple Interest is: " + (principal * rate * time) / 100);
-        }
-}
--- powercalculation.java
+++
@@ -1,7 +0,0 @@
-public class powercalculation {
-    public static void main(String[] args) {
-        int base = 2;
-        int exponent = 3;
-        System.out.println(Math.pow(base, exponent));
-    }
-}
